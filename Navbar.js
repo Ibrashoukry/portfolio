fc576ffc10fdcd7821fa646ed6dab22bbfc21b8f
Ibrahim Shoukry <ibrahim@Ibrahims-MacBook-Air.local>
2024-01-24 01:53:06 -0800

    first commit

---
+++ Navbar.js
@@ -0,0 +1,31 @@
+
+document.getElementById('personal').addEventListener('click', toActive);
+document.getElementById('projects').addEventListener('click', toActive);
+document.getElementById('contact').addEventListener('click', toActive);
+
+slider = document.getElementById('slide');
+personal = document.getElementById('personal');
+projects = document.getElementById('projects');
+contact = document.getElementById('contact');
+root = document.querySelector(':root');
+
+collection = document.getElementsByClassName('tabs');
+all = Array.prototype.slice.call(collection);
+
+function toActive() {
+    /* this part removes the active class 
+    from all the tabs before applying it to the
+    selected tab */
+    all.forEach(element => {
+        element.classList.remove("active");
+    })
+
+    /* this part changes the location of the slider according
+    to which tab is being selected */
+    if (this === personal) { slider.style.left = '0%'; }
+    else if (this === projects) { slider.style.left = '33%'; }
+    else { slider.style.left = '66%'; }
+
+    /* this activates the active class */
+    this.classList.add('active');
+}
