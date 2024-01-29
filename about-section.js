9a6f6e20a06b09772232054bd92e52068dc8264f
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2024-01-28 22:43:49 -0800

    about section and text centering

--- about-section.js
+++ about-section.js
@@ -1,7 +1,13 @@
-const elementsToFadeIn = document.querySelectorAll(".invisible");
+const elementsToFadeIn = document.querySelectorAll(".fade");
 if (elementsToFadeIn) {
   window.addEventListener("scroll", function(event) {
     elementsToFadeIn.forEach(function(element) {
+      const check = window.scrollY;
+      if (check < 100) {
+        document.getElementById("about").classList.add("invisible");
+      } else {
+        document.getElementById("about").classList.remove("invisible");
+      }
       if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
         element.classList.add("fade-in-up");
       } else {
